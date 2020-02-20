cda8ca0f2d541bfbd3dd4435c3621b993c13be5b
Mands <ansc@cin.ufpe.br>
2020-02-20 17:37:45 -0300

    loops implementation

--- sketch.py
+++ sketch.py
@@ -1,17 +1,27 @@
 print("********************************","Welcome to our guessing game!", "********************************", sep = "\n")
 secret_number = 42
+total_tries = 3;
+current_turn = 0;
 
-try1 = int(input("tell us your first guess"))
-print("You said: ", try1)
-its_right= try1 == secret_number
-its_higher= try1 > secret_number
 
-if (its_right):
-        print("you actually got it right!! congrats (:")
-else:
-        if (its_higher):
-                print ("yeah... not this time. your try is now crossing the sky on it's way to the moon. wanna try again? please, lower this time")
+while(total_tries > 0):
+        current_turn = current_turn + 1
+        print("this is your turn number {}, be aware you just have {} more tries.".format(current_turn, total_tries))
+        total_tries = total_tries - 1
+
+        current_try = int(input("tell us your guess"))
+        print("You said: ", current_try)
+
+        its_right= current_try == secret_number
+        its_higher= current_try > secret_number
+
+        if (its_right):
+                print("you actually got it right!! congrats (:")
         else:
-                print("this was a good try!! for a ant. please, try again (more like an elephant this time).")
+                if (its_higher):
+                        print ("yeah... not this time. your try is now crossing the sky on it's way to the moon. wanna try again? please, lower this time")
+                else:
+                        print("this was a good try!! for an ant. please, try again (more like an elephant this time).")
+
 
 print("no more tries for you, buddie")
