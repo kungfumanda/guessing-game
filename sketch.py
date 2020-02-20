2a2ea523872977132ed3b6b00806e644d9f17fbe
Mands <ansc@cin.ufpe.br>
2020-02-20 17:07:41 -0300

    sketch att

--- sketch.py
+++ sketch.py
@@ -2,11 +2,16 @@ print("********************************","Welcome to our guessing game!", "*****
 secret_number = 42
 
 try1 = int(input("tell us your first guess"))
-print ("You said: ", try1)
+print("You said: ", try1)
+its_right= try1 == secret_number
+its_higher= try1 > secret_number
 
-if (try1 == secret_number):
+if (its_right):
         print("you actually got it right!! congrats (:")
 else:
-        print ("yeah... not this time. wanna try again?")
+        if (its_higher):
+                print ("yeah... not this time. your try is now crossing the sky on it's way to the moon. wanna try again? please, lower this time")
+        else:
+                print("this was a good try!! for a ant. please, try again (more like an elephant this time).")
 
 print("no more tries for you, buddie")
